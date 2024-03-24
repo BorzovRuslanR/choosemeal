e75c24ea3f2ddf7812f9abb4b7cffb3e28405635
RuslanB <menejer.r@yandex.com>
2024-03-24 16:36:30 +0300

    add title

--- src/main.tsx
+++ src/main.tsx
@@ -8,18 +8,3 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     <App />
   </React.StrictMode>,
 )
-
-
-{/* <Select
-defaultValue={selectedCuisine}
-onValueChange={(event) => setSelectedCuisine(event.target.value)}
->
-<SelectTrigger>
-  <SelectValue placeholder="Choose cuisine" />
-</SelectTrigger>
-<SelectContent>
-  <SelectItem value="">All</SelectItem>
-  <SelectItem value="Американская кухня">Американская кухня</SelectItem>
-  <SelectItem value="Русская кухня">Русская кухня</SelectItem>
-</SelectContent>
-</Select> */}
